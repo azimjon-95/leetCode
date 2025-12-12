6a5ff50d695af57f08c7597640b2a26aabaff453
azimjon-95 <azimjon995m@gmail.com>
2025-12-12 10:04:27 +0500

    Vowel count, Faktoriyal (n!), Prime number

--- LeetCode.js
+++ LeetCode.js
@@ -1189,20 +1189,20 @@
 
 // console.log(factorial(5));
 
-// // ==========================================================================================
-// // Prime number tekshirish
-// // ==========================================================================================
+// ==========================================================================================
+// Prime number tekshirish
+// ==========================================================================================
 
-// const isPrime = (n) => {
-//     if (n <= 1) return false;
-//     if (n === 2) return true;
+const isPrime = (n) => {
+    if (n <= 1) return false;
+    if (n === 2) return true;
 
-//     for (let i = 2; i < n; i++) {
-//         if (n % i === 0) {
-//             return false;
-//         }
-//     }
-//     return true
-// }
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true
+}
 
-// console.log(isPrime(7));
+console.log(isPrime(7));
